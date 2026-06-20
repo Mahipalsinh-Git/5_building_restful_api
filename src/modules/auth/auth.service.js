536f7f3a6e08cf43d3fd354cf3e2303962e463bd
mahipal <mahipalsinh.thakor@gmail.com>
2026-06-20 10:26:02 +0530

    done - auth service

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -3,6 +3,7 @@ import {
   generateAccessToken,
   generateRefreshToken,
   generateResetToken,
+  verifyRefreshToken,
 } from "../../common/utils/jwt.utils.js";
 import User from "./auth.model.js";
 
@@ -71,4 +72,72 @@ const login = async ({ email, password }) => {
   return { user: userObj, accessToken, refreshToken };
 };
 
-export { registerService };
+const refresh = async ({ refreshToken }) => {
+  if (!refreshToken) {
+    throw APIError.unauthorized("Refresh token not found");
+  }
+
+  const decoded = verifyRefreshToken(refreshToken);
+
+  const user = await User.findById(decoded.id).select("+refreshToken"); // access id from decoded value
+  if (!user) {
+    throw APIError.unauthorized("User not found!");
+  }
+
+  if (user.refreshToken !== hashToken(token)) {
+    throw APIError.unauthorized("Invalid refresh token");
+  }
+
+  const accessToken = generateAccessToken({
+    id: user._id,
+  });
+
+  const refreshToken = generateRefreshToken({
+    id: user._id,
+  });
+
+  user.refreshToken = hashToken(refreshToken);
+  await user.save({ validateBeforeSave: false }); // don't check before save because no need to verify
+
+  const userObj = user.toObject();
+  delete userObj.password;
+  delete userObj.refreshToken;
+
+  return { user: userObj, accessToken, refreshToken };
+};
+
+const logout = async (userId) => {
+  // Way 1
+  // const user = await User.findById(userId); // access id from decoded value
+  // if (!user) {
+  //   throw APIError.unauthorized("User not found!");
+  // }
+
+  // user.refreshToken = undefined;
+  // await user.save({ validateBeforeSave: true });
+
+  // Way 2
+  await User.findByIdAndUpdate(userId, { refreshToken: null });
+};
+
+const forgotPassword = async (email) => {
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    throw APIError.unauthorized("User not found!");
+  }
+
+  const { rawToken, hashedToken } = generateResetToken();
+  user.resetPasswordToken = hashedToken;
+  user.resetPasswordExpires = Date.now() + 15 * 60 * 1000;
+
+  await user.save();
+
+  //TODO: sent mail to user with raw token
+};
+
+export { registerService, login, refresh };
+
+//TODO: After register user, create service for verify token - for that sent email with hash token and also save this token in DB
+// and whenever compare both this token then update "isVerify" status in DB
+
+//TODO: Same as forgot password, and reset password with token
