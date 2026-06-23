1ec5d8e8a89dd76e8996652f3136c3c1a78c3b6e
mahipal <mahipalsinh.thakor@gmail.com>
2026-06-23 15:11:34 +0530

    fix minor bugs

--- src/modules/auth/auth.model.js
+++ src/modules/auth/auth.model.js
@@ -54,11 +54,9 @@ const userSchema = new mongoose.Schema(
 );
 
 // mongoose middleware
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-
+userSchema.pre("save", async function () {
+  if (!this.isModified("password"));
   this.password = await bcrypt.hash(this.password, 12); // salt number between 8 to 12
-  next();
 });
 
 // compare password - bcrypt
